cd4f91cb253ba7785ecd38acc71b61cc127b26d9
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-17 17:03:05 -0300

    (tests)✔: finish tests

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const newtask = {
+      id: Math.random() * 100,
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    if (!newtask.title) return;
+
+    setTasks([...tasks, newtask]);
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const markTasks = tasks.map(task => task.id === id ?{
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+    
+    setTasks(markTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const removeTask =   tasks.filter(task => task.id !== id);
+    
+    setTasks(removeTask);
   }
 
   return (
